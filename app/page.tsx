fa050fb2bd7a9de5f484a4e24484a7312f17ed25
Peter Gamali <petrugamali@gmail.com>
2022-11-29 15:20:05 +0200

    Added fetching functionality for todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import '../styles/global.css'
 
-export default function page() {
+const getTodos = async () => {
+  const todos = await fetch('/api/todos')
+
+  return todos.json()
+}
+
+export default async function page() {
+  const todos = await getTodos()
   return (
     <div>page</div>
   )
