e0e9fc4cffe3c1cc35dcc2feab003657d33a1d58
Peter Gamali <petrugamali@gmail.com>
2022-12-04 22:20:26 +0200

    Added error handling to API routes

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,4 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 import userHandler from '../../../backend/handlers/UserHandler'
 import bcrypt from 'bcrypt'
@@ -7,27 +6,35 @@ const prisma = new PrismaClient()
 
 export default userHandler
     .post(async (req, res) => {
-        const newUser = JSON.parse(req.body)
-        const { password } = newUser
-        const encryptedPassword = await bcrypt.hash(password, 2)
+        try {
+            const newUser = JSON.parse(req.body)
+            const { password } = newUser
+            const encryptedPassword = await bcrypt.hash(password, 2)
 
-        const data = await prisma.user.create({
-            data: {
-                email: newUser.email,
-                password: encryptedPassword
-            }
-        })
+            const data = await prisma.user.create({
+                data: {
+                    email: newUser.email,
+                    password: encryptedPassword
+                }
+            })
 
-        res.status(200).json({ message: "User created." })
+            res.status(200).json({ message: "User created." })
+        } catch (e) {
+            res.status(400).json({ message: e })
+        }
     })
     .get(async (req, res) => {
-        const users = await prisma.user.findMany({
-            select: {
-                id: true,
-                email: true,
-                name: true
-            }
-        })
+        try {
+            const users = await prisma.user.findMany({
+                select: {
+                    id: true,
+                    email: true,
+                    name: true
+                }
+            })
 
-        res.status(200).json({ message: "User list retrieved", ...users })
+            res.status(200).json({ message: "User list retrieved", ...users })
+        } catch (e) {
+            res.status(400).json({ message: e })
+        }
     })
