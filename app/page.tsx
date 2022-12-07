62a7f576753567466fadf6ff077a01561bd3532a
Peter Gamali <petrugamali@gmail.com>
2022-12-07 22:43:10 +0200

    Added authorization API route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { unstable_getServerSession } from 'next-auth/next'
 import React from 'react'
 import '../styles/global.css'
 
 export default async function page() {
+  const session = await unstable_getServerSession()
+  console.log(session)
   return (
     <div>page</div>
   )
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Sign in with credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "Email" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const prisma = new PrismaClient()
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.email
+                    },
+                    select: {
+                        id: true,
+                        email: true,
+                        password: true
+                    }
+                })
+                if (credentials?.password && user) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, user.password)
+                    if (passwordValidation) {
+                        return { id: user.id.toString(), email: user.email }
+                    }
+                }
+                return null
+            }
+        })
+    ]
+}
+
+export default NextAuth(authOptions)
